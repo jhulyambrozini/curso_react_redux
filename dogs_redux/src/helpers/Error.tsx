43942b253653cc56b5054797e7b63b2a18a51e99
Jhuly Ambrozini <102263838+jhulyambrozini@users.noreply.github.com>
2023-09-26 21:30:06 -0300

    Delete dogs_redux directory

--- dogs_redux/src/helpers/Error.tsx
+++
@@ -1,6 +0,0 @@
-const Error = ({ error }: { error: string | null }) => {
-  if (!error) return null;
-  return <p style={{ color: '#f31', margin: '1rem 0' }}>{error}</p>;
-};
-
-export default Error;
